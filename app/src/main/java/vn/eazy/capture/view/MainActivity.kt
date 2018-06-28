dcc49dfb865c2f0f77e6ceaedbf115bf735a495b
LongLe <longle@teqnolgical.asia>
2018-06-28 16:07:16 +0700

    cache file

--- app/src/main/java/vn/eazy/capture/view/MainActivity.kt
+++ app/src/main/java/vn/eazy/capture/view/MainActivity.kt
@@ -1,12 +1,27 @@
 package vn.eazy.capture.view
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //TODO ignoreViews by ids
+//        layout_target?.ignoreViews = arrayListOf(R.id.iv_avatar)
+
+        //TODO getBitmap
+//        layout_target?.getCacheBitmap { iv_captured?.setImageBitmap(it) }
+
+        //TODO get File
+        layout_target?.getCacheFile {
+            it?.apply {
+                Picasso.get().load(it).into(iv_captured)
+            }
+        }
+
     }
 }
